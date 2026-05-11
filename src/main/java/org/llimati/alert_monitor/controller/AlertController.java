17e6a5fce431534a95d5f4444219979c92fc56ab
limati2001 <llimati2001@gmail.com>
2026-05-11 19:31:31 -0300

    Controller added

---
+++ src/main/java/org/llimati/alert_monitor/controller/AlertController.java
@@ -0,0 +1,26 @@
+package org.llimati.alert_monitor.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.llimati.alert_monitor.model.Alert;
+import org.llimati.alert_monitor.service.AlertService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/alerts")
+@RequiredArgsConstructor
+public class AlertController {
+
+    private final AlertService alertService;
+
+    @PostMapping
+    public Alert createAlert(@RequestBody Alert alert) {
+        return alertService.createAlert(alert);
+    }
+
+    @GetMapping
+    public List<Alert> getAllAlerts() {
+        return alertService.getAllAlerts();
+    }
+}
