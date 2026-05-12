8bb05ec019118e63ccea362b15b481ce42f0d937
limati2001 <llimati2001@gmail.com>
2026-05-12 16:33:53 -0300

    SMTP alert service added

--- src/main/java/org/llimati/alert_monitor/service/AlertService.java
+++ src/main/java/org/llimati/alert_monitor/service/AlertService.java
@@ -2,7 +2,10 @@ package org.llimati.alert_monitor.service;
 
 import lombok.RequiredArgsConstructor;
 import org.llimati.alert_monitor.model.Alert;
+import org.llimati.alert_monitor.model.Severity;
 import org.llimati.alert_monitor.repository.AlertRepository;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -11,12 +14,27 @@ import java.util.List;
 @Service
 public class AlertService {
     private final AlertRepository alertRepository;
+    private final JavaMailSender mailSender;
 
     public Alert createAlert (Alert alert){
-        return alertRepository.save(alert);
+        Alert saved = alertRepository.save(alert);
+
+        if (alert.getSeverity() == Severity.CRITICAL){
+            sendCriticalAlertEmail(saved);
+        }
+        return saved;
     }
 
     public List<Alert> getAllAlerts() {
         return alertRepository.findAll();
     }
+
+    private void sendCriticalAlertEmail(Alert alert) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo("llimati2001@gmail.com");
+        message.setSubject("CRITICAL Alert: " + alert.getSource());
+        message.setText("Alert message: " + alert.getMessage() + "\nSource: " + alert.getSource() + "\nTime: " + alert.getCreatedAt());
+        mailSender.send(message);
+    }
+
 }
